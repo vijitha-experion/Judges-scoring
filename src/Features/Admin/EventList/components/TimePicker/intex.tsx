4b5db1c82ca86afae303fba1fdcde0f7c4f3d95b
Vijitha P <vijitha.p@experionglobal.local>
2025-02-28 13:59:09 +0530

    features/program-edit

--- src/Features/Admin/EventList/components/TimePicker/intex.tsx
+++ src/Features/Admin/EventList/components/TimePicker/intex.tsx
@@ -4,8 +4,8 @@ import "react-time-picker/dist/TimePicker.css";
 export function TimePickers({ fieldValues, setFieldValues }: any) {
   return (
     <TimePicker
-      value={fieldValues?.time} 
-      onChange={(value) => setFieldValues("time", value)} 
+      value={fieldValues?.time ?? ""}
+      onChange={(value) => setFieldValues("time", value)}
       disableClock={true}
       className="w-48"
     />
