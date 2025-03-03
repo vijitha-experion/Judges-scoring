cc35f8d4956f62c2b7e4cf6dd49c8533e4fe0e48
Vijitha P <vijitha.p@experionglobal.local>
2025-03-03 20:15:59 +0530

    features/judge-delete

--- src/Features/Admin/JudgesList/intex.tsx
+++ src/Features/Admin/JudgesList/intex.tsx
@@ -4,13 +4,16 @@ import { Button } from "@headlessui/react";
 
 import { TableGrid } from "../../../components/Grid/intex";
 import AddJudge from "./components/addJudge";
+import { DialogBox } from "../../../components/Dialog/intext";
 
 import { judgesListHead } from "./Utils/tableHead";
 import { JudgesType } from "./Types/judgesType";
 import { useJudges } from "./Store/judgesStore";
 
 export function JudgesList(): ReactElement {
-  let [isOpen, setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
+  const [isDelete, setIsDelete] = useState(false);
+  const [selectedRow, setSelectedRow] = useState<JudgesType | null>(null);
 
   const clearJudgesValue = useJudges(
     useCallback((state) => state.clearJudgesValue, [])
@@ -31,7 +34,26 @@ export function JudgesList(): ReactElement {
     setIsOpen(true);
   }
 
-  function onDelete() {}
+  function handleDelete(row: JudgesType) {
+    setSelectedRow(row);
+    setIsDelete(true);
+  }
+
+  function handleDeleteClose() {
+    setIsDelete(false);
+    setSelectedRow(null);
+  }
+
+  function onConfirmDelete() {
+    if (selectedRow) {
+      const updatedEvents = judgesDetails.filter(
+        (judge: JudgesType) => judge.phone !== selectedRow?.phone
+      );
+      localStorage.setItem("judgesDetails", JSON.stringify(updatedEvents));
+      setIsDelete(false);
+      setSelectedRow(null);
+    }
+  }
   return (
     <div className="pl-14 mr-14">
       <div className="flex justify-between items-center pt-10">
@@ -50,10 +72,21 @@ export function JudgesList(): ReactElement {
         totalPages={3}
         onPageChange={(page) => console.log("Go to page:", page)}
         onRowClick={() => {}}
-        onDelete={onDelete}
+        onDelete={handleDelete}
         onEdit={onEdit}
       />{" "}
       {isOpen && <AddJudge isOpen={isOpen} handleClose={handleClose} />}
+      {isDelete && (
+        <DialogBox
+          title="Delete Event"
+          description="Are you sure you want to delete this judge details?"
+          button1="Cancel"
+          button2="Delete"
+          opened={isDelete}
+          handleClose={handleDeleteClose}
+          handleAction={onConfirmDelete}
+        />
+      )}
     </div>
   );
 }
